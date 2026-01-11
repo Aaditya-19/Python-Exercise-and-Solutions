6b17ccbf8347e4eae63cd0de4aa9b42032fc4e40
Aaditya-19 <139311241+Aaditya-19@users.noreply.github.com>
2026-01-11 22:04:10 +0530

    Update and rename main.py to random_program_practice.py

---
+++ random_program_practice.py
@@ -0,0 +1,16 @@
+# 1. PROGRAM TO PRINT MERRY CHRISTMAS WITH SOME NICE EFFECTS
+
+import time
+fire =   ["⭐","💥","🔔"]
+for i in range(5,0,-1):
+    print(f"{i} 🎄")
+    time.sleep(0.5)
+
+for f in fire:
+    print(f * 10)
+    time.sleep(0.3)
+
+print("MERRY CHRISTMAS")
+
+
+
